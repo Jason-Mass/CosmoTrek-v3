4731d962f481795e7bcb45a48f4b631025e5b39c
55645nato <samuel.w.henry@outlook.com>
2022-04-22 15:44:06 -0500

    Added migration

--- Data/Migrations/20220422204250_ModelsAdded.Designer.cs
+++ Data/Migrations/20220422204250_ModelsAdded.Designer.cs
@@ -4,6 +4,7 @@ using CosmoTrek_v3.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CosmoTrek_v3.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220422204250_ModelsAdded")]
+    partial class ModelsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
