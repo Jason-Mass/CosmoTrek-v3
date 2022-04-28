9ea5413d4ceb91d9c31a50738a523fec671ea151
55645nato <samuel.w.henry@outlook.com>
2022-04-28 13:31:34 -0500

    Changed model relationships

--- Models/TrekReservation.cs
+++ Models/TrekReservation.cs
@@ -19,12 +19,5 @@ namespace CosmoTrek_v3.Models
         public string Phone { get; set; }
         [EmailAddress]
         public string Email { get; set; }
-
-        public SpaceTravelIdentityUser SpaceTravelUser { get; set; } // logical navigation property
-        public string SpaceTravelUserId { get; set; } // string is default data type of ID because that corresponds to IdentityUser class Id property
-        public TrekPlan TrekPlan { get; set; }
-        public int TrekPlanId { get; set; }
-
-
     }
 }
